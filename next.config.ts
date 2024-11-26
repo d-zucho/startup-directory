de39c8820b846719ce6c5560bb3886e9b79d10d9
D_Zucho <danny.nico.dev@outlook.com>
2024-11-26 02:07:17 -0800

    add startup card component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import { hostname } from 'os'
+import { RemotePattern } from './node_modules/next/dist/shared/lib/image-config.d'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
